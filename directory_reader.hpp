8c4d41e048f9f4e800c53c7efe86f1ef0b2d1040
Karl Essinger <xkaess22@gmail.com>
2020-04-12 00:16:17 +0200

    Fixed some linux incompatibility

---
+++ directory_reader.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+#ifdef _WIN32 // Windows only
+    #include <filesystem>
+#else // Unix only
+    #include <dirent.h>
+#endif
+
+class DirectoryReader {
+public:
+    static std::vector<std::string> getFilePaths(const std::string path);
+};
+
