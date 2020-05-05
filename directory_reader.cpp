916fe5fc57e863e00f9770ceca0e495bd1bec14e
Karl Essinger <xkaess22@gmail.com>
2020-05-06 01:12:44 +0200

    Fixed exception on windows

--- directory_reader.cpp
+++ directory_reader.cpp
@@ -6,20 +6,25 @@ std::vector<std::string> DirectoryReader::getFilePaths(const std::string path)
 {
     std::vector<std::string> fileList = {};
 
-    //Get the files in alphabetical order aka sorted by time
-    for (auto& file : std::filesystem::directory_iterator(path))
-    {
-        if (std::filesystem::is_regular_file(file))
-        {
-            const size_t dot = file.path().string().find_last_of('.');
-            std::string fileExt = file.path().string().substr(dot, file.path().string().size() - dot);
-            if (fileExt == ".log")
-            {
-                fileList.push_back(file.path().string());
-                std::cout << " Found " << file.path().string() << "'\n";
-            }
-        }
-    }
+	try
+	{
+		//Get the files in alphabetical order aka sorted by time
+	    for (auto& file : std::filesystem::directory_iterator(path))
+	    {
+	        if (std::filesystem::is_regular_file(file))
+	        {
+	            const size_t dot = file.path().string().find_last_of('.');
+	            std::string fileExt = file.path().string().substr(dot, file.path().string().size() - dot);
+	            if (fileExt == ".log")
+	            {
+	                fileList.push_back(file.path().string());
+	                std::cout << " Found " << file.path().string() << "'\n";
+	            }
+	        }
+	    }
+	}
+	catch (std::filesystem::filesystem_error&) { return {}; }
+
     return fileList;
 }
 
